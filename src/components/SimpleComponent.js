79e757ce8678cf22e7b02dacf40043777711080b
myildiz17 <muhammetyildiz@gmail.com>
2021-02-08 16:41:08 -0600

    complete

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,30 @@
 // Code SimpleComponent Here
+import React, { Component } from "react";
+
+class SimpleComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mood: "sad",
+    };
+  }
+
+  handleClicks = () => {
+    if (this.state.mood === "sad") {
+      this.setState({
+        mood: "happy",
+      });
+    } else {
+      this.setState({
+        mood: "sad",
+      });
+    }
+  };
+  
+
+  render() {
+    return <div onClick={this.handleClicks}>{this.state.mood}</div>;
+  }
+}
+
+export default SimpleComponent;
